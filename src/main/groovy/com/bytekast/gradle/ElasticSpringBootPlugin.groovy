8ae345dcff07e7bca629dfa85c1407b8564ff695
Rowell Belen <developer@bytekast.com>
2017-02-27 21:23:57 -0700

    Skeleton Project

---
+++ src/main/groovy/com/bytekast/gradle/ElasticSpringBootPlugin.groovy
@@ -0,0 +1,12 @@
+package com.bytekast.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ElasticSpringBootPlugin implements Plugin<Project> {
+
+  @Override
+  void apply(Project project) {
+
+  }
+}
